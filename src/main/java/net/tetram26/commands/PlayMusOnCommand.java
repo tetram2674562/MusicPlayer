6466a1f3ab8d51145575058811444b17ad2198d6
tetram2674562 <54166742+tetram2674562@users.noreply.github.com>
2025-05-17 00:18:16 +0200

    Legal : Added legal notice on top of each source file for my work

--- src/main/java/net/tetram26/commands/PlayMusOnCommand.java
+++ src/main/java/net/tetram26/commands/PlayMusOnCommand.java
@@ -1,3 +1,5 @@
+// Copyright (c) 2024-2025 tetram2674562
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 package net.tetram26.commands;
 
 import java.util.List;
